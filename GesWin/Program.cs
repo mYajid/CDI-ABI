980c98ab5439a77d8a8417c05fe8e5c5f8230cbc
mYajid <m.yajid@microlan.fr>
2018-08-05 19:20:28 +0200

    debug classes client et fonction fonction enum

--- GesWin/Program.cs
+++ GesWin/Program.cs
@@ -17,17 +17,24 @@ namespace GesWin
         {
 
             HashSet<Client> clientsHS = new HashSet<Client>();
-
+            HashSet<Contact> contactsHS = new HashSet<Contact>();
             Client client1 = new Client();
+            client1.IdClient = 1234;
             client1.RaisonSociale = "Exploitation Legoff";
             client1.Adresse1 = "Le Hameau";
             client1.Adresse2 = "Route de Ploubazlanec";
             client1.CodPostal = 22500;
             client1.Ville = "Paimpol";
             client1.TelephoneClient = 0299010203;
-            client1.ListContact=;
+            client1.ListContact = contactsHS;
 
-
+            Contact contact1=new Contact() ;
+            contact1.IdClient= 1234;
+            contact1.NomContact = "Neymar";
+            contact1.PrenomContact = "Jean";
+            contact1.Telephone = 0101015555;
+            contact1.FonctionSetting = Fonction.Commercial;
+            contactsHS.Add(contact1);
 
             //Contact contact1 = new Contact();
             //contact1.Client.

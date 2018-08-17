48bd7bbef083464c4737c975f40a54d6a1947a12
mYajid <m.yajid@microlan.fr>
2018-08-17 14:30:46 +0200

    Création Class File pour la sérialisation du fichier clients.

---
+++ GestionCommercialeDll/File.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionCommercialeDll
+{
+    class File
+    {
+        //Méthode pour écrire la collection clientsHS sur le disque.
+        public static void WriteToFile( )
+        {
+            // Conversion du HashSet en list
+            
+            List<Client> clientList = new List<Client>(Client.clientsHS);
+          
+        }
+
+        public static void ReadFromFile()
+        {
+
+
+            HashSet<Client> tempHS = new HashSet<Client>(clientList);
+        }
+    }
+}

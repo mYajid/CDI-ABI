cee082afbde1703d4d9d7e4433854ce0c2868bc9
Svetlana <sv.ts@neuf.fr>
2018-08-23 09:02:43 +0200

    activation serialization

--- GesWin/ListClients.cs
+++ GesWin/ListClients.cs
@@ -48,7 +48,7 @@ namespace GesWin
 
         private void btnFermerListeClient_Click(object sender, EventArgs e)
         {
-           //Fichier.WriteToFile();
+           Fichier.WriteToFile();
             this.Close();
         }
 
